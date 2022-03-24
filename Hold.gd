07b148a1e9e44a7e919b5d8f84e5b7e76a8e216a
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-03-24 11:13:57 +0000

    Create Hold

---
+++ Hold.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+var mino : Mino = null setget set_mino
+
+
+func _draw() -> void:
+	draw_rect(Rect2(position, Vector2(80, 80)), Color.gray)
+
+
+func swap_mino(new_mino : Mino) -> Mino:
+	var old = mino
+	set_mino(new_mino)
+	return old
+
+
+func set_mino(mino_ : Mino) -> void:
+	if mino:
+		remove_child(mino)
+
+	if mino_:
+		mino_.position = Vector2.ZERO
+		add_child(mino_)
+
+	mino = mino_
