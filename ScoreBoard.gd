19566e26f741af094bdbcd40f4100df00b59c146
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-03-28 11:37:37 +0100

    Actually display scores on ScoreBoard

--- ScoreBoard.gd
+++ ScoreBoard.gd
@@ -4,7 +4,14 @@ extends Node2D
 func _ready() -> void:
 	# warning-ignore: RETURN_VALUE_DISCARDED
 	ScoreManager.connect("scores_updated", self, "_on_ScoreManager_scores_updated")
+	update_labels()
+
+
+func update_labels() -> void:
+	$Panel/ScoreLabel.text = "Score: %d" % ScoreManager.current_score
+	$Panel/HiScoreLabel.text = "Score: %d" % ScoreManager.high_score
+	$Panel/LevelLabel.text = "Score: %d" % ScoreManager.current_level
 
 
 func _on_ScoreManager_scores_updated() -> void:
-	print(ScoreManager.current_score)
+	update_labels()
