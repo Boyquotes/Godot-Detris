67c9365a1f57bb111364b553ad31f8baec318ccf
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-03-28 12:01:22 +0100

    Give minos in Hold a margin

--- Hold.gd
+++ Hold.gd
@@ -13,8 +13,10 @@ func swap_mino(new_shape : int) -> int:
 		mino = Mino.new()
 		add_child(mino)
 		mino.set_shape(new_shape)
+		mino.position = Vector2.ONE * 8
 		return 0
 	else:
 		var old_shape = mino.shape
 		mino.set_shape(new_shape)
+		mino.position = Vector2.ONE * 8
 		return old_shape
