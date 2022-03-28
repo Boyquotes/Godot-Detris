298b0b70f501563cb7dfb146203e1c2da5bde94f
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-03-28 17:31:55 +0100

    Show high score on title screen

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -7,6 +7,7 @@ signal game_started ()
 func _ready() -> void:
 	# warning-ignore: RETURN_VALUE_DISCARDED
 	connect("game_started", Global, "_on_TitleScreen_game_started")
+	$Control/HiScore.text = "Hi: %07d" % Global.high_score
 
 
 func _input(event : InputEvent) -> void:
@@ -16,16 +17,16 @@ func _input(event : InputEvent) -> void:
 	):
 		set_process_input(false)
 		$AudioStreamPlayer.play()
-		$Control/Label.hide()
+		$Control/StartLabel.hide()
 		$BlinkTimer.start()
 		$StartTimer.start()
 
 
 func _on_BlinkTimer_timeout() -> void:
-	if $Control/Label.visible:
-		$Control/Label.hide()
+	if $Control/StartLabel.visible:
+		$Control/StartLabel.hide()
 	else:
-		$Control/Label.show()
+		$Control/StartLabel.show()
 
 
 func _on_StartTimer_timeout() -> void:
