96535e73fc113935b91732cf19d43b91c2c67225
Tori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2022-03-29 10:42:56 +0100

    Keep track of lines cleared, and increment level accordingly

--- Global.gd
+++ Global.gd
@@ -2,6 +2,8 @@ extends Node
 
 
 signal scores_updated
+signal lines_updated
+signal level_updated
 
 
 export var points_single := 100
@@ -9,9 +11,11 @@ export var points_double := 400
 export var points_triple := 900
 export var points_detris := 1600
 export var points_harddrop := 10
+export var lines_per_level := 10
 
 var current_score := 0 setget set_score
 var current_level := 0
+var current_lines := 0 setget set_lines
 var high_score := 0
 
 var title_scene : PackedScene = preload("res://TitleScreen.tscn")
@@ -20,10 +24,21 @@ var game_scene : PackedScene = preload("res://Playfield.tscn")
 
 func _init() -> void:
 	randomize()
+	reset()
+
+
+func reset() -> void:
+	set_score(0)
+	set_lines(0)
+	current_level = 0
 
 
 func increment_score(points : int) -> void:
 	set_score(current_score + points)
+
+
+func increment_lines(lines : int) -> void:
+	set_lines(current_lines + lines)
 
 
 func set_score(score_ : int) -> void:
@@ -33,11 +48,22 @@ func set_score(score_ : int) -> void:
 	emit_signal("scores_updated")
 
 
+func set_lines(lines_ : int) -> void:
+	current_lines = lines_
+	emit_signal("lines_updated")
+	# warning-ignore: INTEGER_DIVISION
+	var target_level = current_lines / lines_per_level
+	if target_level > current_level:
+		current_level = target_level
+		emit_signal("level_updated")
+
+
 func _on_Matrix_hard_dropped() -> void:
 	increment_score(points_harddrop)
 
 
 func _on_Matrix_lines_cleared(lines : int) -> void:
+	increment_lines(lines)
 	match lines:
 		1:
 			increment_score(points_single)
@@ -57,6 +83,6 @@ func _on_TitleScreen_game_started() -> void:
 
 
 func _on_Matrix_game_lost() -> void:
+	reset()
 	# warning-ignore: RETURN_VALUE_DISCARDED
-	set_score(0)
 	get_tree().change_scene_to(title_scene)
--- ScoreBoard.gd
+++ ScoreBoard.gd
@@ -4,14 +4,27 @@ extends Node2D
 func _ready() -> void:
 	# warning-ignore: RETURN_VALUE_DISCARDED
 	Global.connect("scores_updated", self, "_on_Global_scores_updated")
+	# warning-ignore: RETURN_VALUE_DISCARDED
+	Global.connect("lines_updated", self, "_on_Global_lines_updated")
+	# warning-ignore: RETURN_VALUE_DISCARDED
+	Global.connect("level_updated", self, "_on_Global_level_updated")
 	update_labels()
 
 
 func update_labels() -> void:
 	$ScoreNumber.text = "%07d" % Global.current_score
 	$HiScoreNumber.text = "%07d" % Global.high_score
+	$LinesNumber.text = "%03d" % Global.current_lines
 	$LevelNumber.text = "%d" % Global.current_level
 
 
 func _on_Global_scores_updated() -> void:
+	update_labels()
+
+
+func _on_Global_lines_updated() -> void:
+	update_labels()
+
+
+func _on_Global_level_updated() -> void:
 	update_labels()
